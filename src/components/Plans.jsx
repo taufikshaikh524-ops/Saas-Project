bf16d37feb0a2c96f00d25f721658f1c489e5ff5
taufikshaikh524-ops <taufikshaikh524@gmail.com>
2026-03-29 15:21:10 +0530

    update

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -5,7 +5,7 @@ const Plans = () => {
         <div className='mx-auto max-w-2xl z-20 my-30'>
             <div className="text-center">
                 <h2 className='text-slate-700 text-[42px]'>Choose Your Plan</h2>
-                <p className='mx-w-lg mx-auto text-grey-500'>Start for free and scale up for your grow.
+                  <p className='mx-w-lg mx-auto text-grey-500'>Start for free and scale up for your grow.
                 Find the perfect plan for your content generation needs.</p>
             </div>
             <div className="mt-10">
